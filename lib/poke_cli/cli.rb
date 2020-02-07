fc0b454ea6da7fe249b9190b992bf8a1643a1eb7
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-07 12:31:12 +0000

    new

--- lib/poke_cli/cli.rb
+++ lib/poke_cli/cli.rb
@@ -3,5 +3,17 @@ class PokeCli::CLI
     puts "Welcome pokemon trainer"
     puts "To use your pokedex and view a pokemon enter: 'pokedex'"
     puts "To continue on your journey enter: 'exit'"
+    menu
   end 
+  def menu 
+    input = gets.strip.downcase
+    if input == "pokedex"
+      puts "pokedex"
+      elsif input == "exit"
+      puts "exit"
+    else 
+      puts "invalid pokedex entry."
+      menu
+  end 
+end 
 end 
