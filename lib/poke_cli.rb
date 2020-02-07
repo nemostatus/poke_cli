b5129ed971f0cb4279065ab0502e631c9feecfab
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-07 12:22:35 +0000

    new

--- lib/poke_cli.rb
+++ lib/poke_cli.rb
@@ -1,3 +1,7 @@
 require_relative "./poke_cli/version"
+require_relative "./poke_cli/api"
+require_relative "./poke_cli/cli"
+require_relative "./poke_cli/pokemon"
+
 
 puts "enviornment"
---
+++ lib/poke_cli/cli.rb
@@ -0,0 +1,5 @@
+class PokeCli::CLI 
+  def call 
+    puts "im in the cli"
+  end 
+end 
