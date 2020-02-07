8211bf4f0fba31f535c4227cce38458185bd2bc3
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-07 12:47:11 +0000

    gems installed

--- lib/poke_cli.rb
+++ lib/poke_cli.rb
@@ -2,6 +2,8 @@ require_relative "./poke_cli/version"
 require_relative "./poke_cli/api"
 require_relative "./poke_cli/cli"
 require_relative "./poke_cli/pokemon"
+require "rest-client"
+require "json"
 
 
 
