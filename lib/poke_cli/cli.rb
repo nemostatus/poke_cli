335dcf0fb6d6f5764c062399208dcb9c3993e595
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-07 12:45:13 +0000

    new

--- lib/poke_cli/cli.rb
+++ lib/poke_cli/cli.rb
@@ -20,6 +20,15 @@ def pokemon_list
   puts "1. pokemon 1"
    puts "2. pokemon 2"
    puts "3. pokemon 3"
+   puts ""
+   puts ""
+   puts "Which pokemon would you like stats about?"
+   input = gets.strip.downcase
+   pokemon_selection(pokemon)
+   
+end 
+def pokemon_selection(pokemon)
+  puts "#{pokemon}"
 end 
   def bye 
     puts "Enjoy your journey and try to catch 'em all!"
