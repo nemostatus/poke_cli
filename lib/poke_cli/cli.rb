45016ae9e229794eb891c380b3a9fb95f9de4aca
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-12 04:35:53 +0000

    y

--- lib/poke_cli/cli.rb
+++ lib/poke_cli/cli.rb
@@ -1,7 +1,7 @@
 class PokeCli::CLI
   
 def call 
-    puts "Welcome pokemon trainer"
+    puts "Welcome pokemon trainer!"
     puts "To use your pokedex and view a pokemon enter: 'pokedex'"
     puts "To continue on your journey enter: 'exit'"
     API.get_data
@@ -32,14 +32,18 @@ def pokemon_list
       puts "Which pokemon would you like to know about?"
     name = gets.strip.downcase
     puts "Which stats would you like to know?"
-    puts  "1. Abilities
+    stat_list
+    stat = gets.strip.downcase
+   puts API.more_data(name,stat)
+   end 
+   
+   def stat_list
+       puts  "1. Abilities
   2.Base_experience
   3.Weight
   4.Height
   5.Moves
   6.Types"
-    stat = gets.strip.downcase
-   puts API.more_data(name,stat)
    end 
   
    def choice 
@@ -59,6 +63,8 @@ def pokemon_list
   
   def bye 
     puts "Enjoy your journey and try to catch 'em all!"
+    
+    
   end
   def invalid_entry
     puts "Invalid pokedex entry."
