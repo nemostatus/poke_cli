f35d1dcd431182930492d7e3b23d81d69a2d08c8
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-12 03:37:52 +0000

    improving

--- lib/poke_cli/cli.rb
+++ lib/poke_cli/cli.rb
@@ -34,9 +34,19 @@ def pokemon_list
   5.Moves
   6.Types"
     stat = gets.strip.downcase
-   
- puts API.more_data(name,stat)
+   puts API.more_data(name,stat)
+   choice
    end
+   def choice 
+     puts "Would you like to learn more about pokemon?"
+     puts "y/n"
+     input = gets.strip.downcase 
+     if input == "y"
+       pokemon_list
+     elsif input == "n"
+     bye 
+   end 
+ end 
   
   def bye 
     puts "Enjoy your journey and try to catch 'em all!"
